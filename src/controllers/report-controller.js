53b9199a64ec49340ec9d2587f9f4501bddbe45c
Raihan <raihanm854@gmail.com>
2025-09-25 14:06:02 +0700

    adjust: name camel case

--- src/controllers/report-controller.js
+++ src/controllers/report-controller.js
@@ -2,7 +2,7 @@ import successResponse from "../responses/success-response.js";
 import ReportService from "../services/report-service.js";
 
 export default class ReportController {
-    static async GetAllKunjungan(req, res, next) {
+    static async getAllKunjungan(req, res, next) {
         try {
             const result = await ReportService.getAllKunjungan(req.query);
             return res.status(200).json(successResponse("Data Kunjungan RI berhasil ditampilkan", result));
@@ -11,7 +11,7 @@ export default class ReportController {
         }
     }
 
-    static async GetCancelKunjungan(req, res, next) {
+    static async getCancelKunjungan(req, res, next) {
         try {
             const result = await ReportService.getCancelKunjungan(req.query);
             return res.status(200).json(successResponse("Data Batal Kunjungan RI berhasil ditampilkan", result));
--- src/routes/router.js
+++ src/routes/router.js
@@ -19,7 +19,7 @@ router.get('/transfer-bed/total-pending', PerpindahanController.GetTotalPending)
 
 
 //* Reports
-router.get('/ranap/report/kunjungan', ReportController.GetAllKunjungan);
-router.get('/ranap/report/batal-kunjungan', ReportController.GetCancelKunjungan);
+router.get('/ranap/report/kunjungan', ReportController.getAllKunjungan);
+router.get('/ranap/report/batal-kunjungan', ReportController.getCancelKunjungan);
 
 export default router;
